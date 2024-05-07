39d5acc367b8ee472c80014872ae681d2ccb62a3
Lingxi Xing <thelastoreointhejar@gmail.com>
2024-05-07 17:16:30 +0800

    Multithread clients

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,79 +1,111 @@
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <poll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <algorithm>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <thread>
+#include <vector>
+
+void handle_client(int index, std::vector<int> &client_sockets);
 
 int main(int argc, char **argv) {
-  // You can use print statements as follows for debugging, they'll be visible
-  // when running tests.
-  std::cout << "Logs from your program will appear here!\n";
+    // You can use print statements as follows for debugging, they'll be visible
+    // when running tests.
+    std::cout << "Logs from your program will appear here!\n";
 
-  // Uncomment this block to pass the first stage
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-    std::cerr << "Failed to create server socket\n";
-    return 1;
-  }
+    // Uncomment this block to pass the first stage
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        std::cerr << "Failed to create server socket\n";
+        return 1;
+    }
 
-  // Since the tester restarts your program quite often, setting SO_REUSEADDR
-  // ensures that we don't run into 'Address already in use' errors
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <
-      0) {
-    std::cerr << "setsockopt failed\n";
-    return 1;
-  }
+    // Since the tester restarts your program quite often, setting SO_REUSEADDR
+    // ensures that we don't run into 'Address already in use' errors
+    int reuse = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        std::cerr << "setsockopt failed\n";
+        return 1;
+    }
 
-  struct sockaddr_in server_addr;
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(6379);
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(6379);
 
-  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) !=
-      0) {
-    std::cerr << "Failed to bind to port 6379\n";
-    return 1;
-  }
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
+        std::cerr << "Failed to bind to port 6379\n";
+        return 1;
+    }
 
-  int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
-    return 1;
-  }
+    int connection_backlog = 5;
+    if (listen(server_fd, connection_backlog) != 0) {
+        std::cerr << "listen failed\n";
+        return 1;
+    }
 
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
+    std::cout << "Waiting for a client to connect...\n";
+    std::vector<int> client_sockets;
+    while (true) {
+        std::vector<pollfd> fds;
+        fds.push_back({server_fd, POLLIN, 0});
+        for (int client_socket : client_sockets) {
+            fds.push_back({client_socket, POLLIN, 0});
+        }
 
-  std::cout << "Waiting for a client to connect...\n";
+        int num_ready = poll(fds.data(), fds.size(), -1);
+        if (num_ready < 0) {
+            std::cerr << "Error in poll\n";
+            exit(1);
+        }
 
-  int client_socket = accept(server_fd, (struct sockaddr *)&client_addr,
-                             (socklen_t *)&client_addr_len);
-  std::cout << "Client connected\n";
+        if (fds[0].revents & POLLIN) {
+            struct sockaddr_in client_addr;
+            socklen_t client_addr_len = sizeof(client_addr);
+            int client_socket = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+            if (client_socket < 0) {
+                std::cerr << "Failed to accept connection\n";
+                exit(1);
+            }
+            std::cout << "New connection accepted\n";
+            client_sockets.push_back(client_socket);
+        }
 
-  char buffer[1024];
-  while (true) {
+        for (size_t i = 1; i < fds.size(); i++) {
+            if (fds[i].revents & POLLIN) handle_client(i - 1, client_sockets);
+        }
+
+        // Clear clients who errored / closed during handling
+        client_sockets.erase(std::remove(client_sockets.begin(), client_sockets.end(), -1), client_sockets.end());
+    }
+
+    close(server_fd);
+    return 0;
+}
+
+void handle_client(int index, std::vector<int> &client_sockets) {
+    char buffer[1024];
+    int client_socket = client_sockets[index];
     int recv_bytes = recv(client_socket, buffer, sizeof(buffer), 0);
 
     if (recv_bytes < 0) {
-      std::cout << "Error receiving bytes\n";
-      break;
+        std::cout << "Error receiving bytes\n";
+        close(client_socket);
+        client_sockets[index] = -1;
     } else if (recv_bytes == 0) {
-      std::cout << "Client disconnected\n";
-      break;
+        std::cout << "Client disconnected\n";
+        close(client_socket);
+        client_sockets[index] = -1;
     } else {
-      std::cout << "Message received: " << buffer << '\n';
-      std::string res = "+PONG\r\n";
-      send(client_socket, res.c_str(), res.size(), 0);
+        std::cout << "Message received: " << buffer << '\n';
+        std::string res = "+PONG\r\n";
+        send(client_socket, res.c_str(), res.size(), 0);
     }
-  }
-
-  close(client_socket);
-  close(server_fd);
-  return 0;
 }
